69a93fb655de373933ffd14c2aa48f7f185470bd
José Pedro <jgomes@geobit.com.br>
2021-08-23 22:33:07 -0300

    validando valor

--- app/__init__.py
+++ app/__init__.py
@@ -9,12 +9,12 @@ from .src.settings import TOKEN, PORT, SITE_URL
 def run():
     bot = Bot(token=TOKEN, use_context=True)
 
-    # bot.start_webhook(
-    #     listen="0.0.0.0",
-    #     port=PORT,
-    #     url_path=TOKEN
-    # )
-    # bot.bot.setWebhook(SITE_URL + TOKEN)
-    bot.start_polling()
+    bot.start_webhook(
+        listen="0.0.0.0",
+        port=PORT,
+        url_path=TOKEN
+    )
+    bot.bot.setWebhook(SITE_URL + TOKEN)
+    # bot.start_polling()
 
     bot.idle()
